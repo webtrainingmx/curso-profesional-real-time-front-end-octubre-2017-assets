7aac3d64a39ae94a714cfe16e7afabe416d29f30
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-10-11 18:56:19 -0500

    Agregar ejemplo con colores para Chart.js

--- src/app/auth/polls-reports/polls-reports.component.ts
+++ src/app/auth/polls-reports/polls-reports.component.ts
@@ -15,15 +15,19 @@ export class PollsReportsComponent implements OnInit {
       labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ],
       datasets: [
         {
-          label: 'First Dataset',
+          label: 'My First dataset',
+          backgroundColor: '#42A5F5',
+          borderColor: '#1E88E5',
           data: [ 65, 59, 80, 81, 56, 55, 40 ]
         },
         {
-          label: 'Second Dataset',
+          label: 'My Second dataset',
+          backgroundColor: '#9CCC65',
+          borderColor: '#7CB342',
           data: [ 28, 48, 40, 19, 86, 27, 90 ]
         }
       ]
-    }
+    };
   }
 
 
