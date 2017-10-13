b0fea001df289a265232786cb3ff023ae69049ed
Alex Serrano <alexserrano63@gmail.com>
2017-10-13 11:48:15 -0500

    Add guards

---
+++ src/app/common/guards/public.guard.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {AuthenticationService} from '../../services/authentication.service';
+
+@Injectable()
+export class PublicGuard implements CanActivate {
+  constructor(public _authenticationService: AuthenticationService,
+              public router: Router) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this._authenticationService.isLoggedIn()) {
+      return true;
+    }
+
+    console.log('you are login!');
+    this.router.navigate(['/home']);
+    return false;
+  }
+}
