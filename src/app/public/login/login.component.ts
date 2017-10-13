abac79627fd731d8ab690fb351873295bc96eca9
Alex Serrano <alexserrano63@gmail.com>
2017-10-13 11:51:13 -0500

    Add login component

---
+++ src/app/public/login/login.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../services/authentication.service';
+import {Router} from '@angular/router';
+import {SessionStorageService} from 'ngx-webstorage';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: any = <any>{};
+  constructor(public _authService: AuthenticationService,
+              public _router: Router,
+              public _locker: SessionStorageService
+  ) {
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    this._authService.logIn(this.user.username, this.user.password).subscribe(
+      (data) => {
+        this._authService.wasLoginSuccessful(true);
+        this._authService.user = data;
+        this._authService.hasSession = true;
+        this._locker.store('user', data);
+        this._router.navigate(['/home']);
+      },
+      err => {
+        console.error(err);
+        this._authService.wasLoginSuccessful(false);
+        this._authService.hasSession = false;
+      }
+    );
+  }
+
+}
